cf3c3d2368d157e7757a7910f9159ed530c87d09
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-09 12:45:14 -0500

    Guard against non-bash shells running completions

--- lib/bash/completion.sh
+++ lib/bash/completion.sh
@@ -1,3 +1,8 @@
+# Only run in interactive bash
+if [[ -z ${BASH_VERSION:-} || $- != *i* ]]; then
+  return 0 2>/dev/null || exit 0
+fi
+
 for f in "$(dirname ${BASH_SOURCE})/completions/"*; do
     . "$f"
 done
