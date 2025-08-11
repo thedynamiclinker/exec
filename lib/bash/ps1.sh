27fd2cdbc218e0d0ffd7cc517c907d925140b128
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-11 18:54:56 -0500

    Change length threshold for clip_prompt_if_its_super_long

--- lib/bash/ps1.sh
+++ lib/bash/ps1.sh
@@ -3,16 +3,17 @@
 clip_prompt_if_its_super_long() {
     # If we're starting-up in a directory with a really-long
     # pathname, then switch to a shorter prompt automatically.
-    if [[ "$(pwd | wc -c)" > 50 ]]; then
+    if [[ "$(pwd | wc -c)" > 30 ]]; then
         export PS1="$(echo "$PS1" | sed 's@w@W@g')";
     fi
 }
 
 # make prompt shorter or longer
-sp() {
+shorter_prompt() {
     export PS1="$(echo "$PS1" | sed "s@w@W@g")"
 }
-lp() {
+longer_prompt() {
     export PS1="$(echo "$PS1" | sed "s@W@w@g")"
 }
-
+alias sp='shorter_prompt'
+alias lp='longer_prompt'
