c77934876c36bdb55b66d42a9e1bf8c58b7aca76
Jason Wilkes <root@thedynamiclinker.com>
2026-01-28 12:26:07 -0600

    Fix bug in clip_prompt_if_its_super_long

--- lib/bash/ps1.sh
+++ lib/bash/ps1.sh
@@ -4,7 +4,7 @@ clip_prompt_if_its_super_long() {
     # If we're starting-up in a directory with a really-long
     # pathname, then switch to a shorter prompt automatically.
     if [[ "$(pwd | wc -c)" > 30 ]]; then
-        export PS1="$(echo "$PS1" | sed 's@w@W@g')";
+        export PS1="$(echo "$PS1" | sed 's@\\w@\\W@g')";
     fi
 }
 
